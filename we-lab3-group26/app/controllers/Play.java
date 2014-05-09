a78c9380ae0a9b3a9fda656750a2e5c038360982
Andreas Wittmann <e1225854@student.tuwien.ac.at>
2014-05-09 21:08:38 +0200

    rundenseite und endseite

--- we-lab3-group26/app/controllers/Play.java
+++ we-lab3-group26/app/controllers/Play.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Random;
 import java.util.Set;
 
 import models.Member;
@@ -12,6 +13,7 @@ import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
+import at.ac.tuwien.big.we14.lab2.api.Answer;
 import at.ac.tuwien.big.we14.lab2.api.Choice;
 import at.ac.tuwien.big.we14.lab2.api.Question;
 import at.ac.tuwien.big.we14.lab2.api.QuizFactory;
@@ -39,14 +41,20 @@ public class Play extends Controller {
 		
 		return newRound();
 	}
+	
+	public static Result nextGame(){
+		return startGame(user);
+	}
 		
 	public static Result newRound(){
 		game.startNewRound(); // start new game/round
 		round = game.getCurrentRound();// current round
+		questionCounter = 0;
 		return nextQuestion();
 	}
 	
 	public static Result nextQuestion(){
+		
 		question = round.getCurrentQuestion(user);
 		
 		List<String> answersOverviewUser = new ArrayList<String>();
@@ -67,9 +75,10 @@ public class Play extends Controller {
 		
 		List<String> answersOverviewComputer = new ArrayList<String>();
 		i = 0;
+				
 		while(i < questionCounter) //Hole die Antworten der schon gegebenen fragen
 		{
-			if(round.getAnswer(i, user).isCorrect())
+			if(round.getAnswer(i, game.getPlayers().get(1)).isCorrect())
 				answersOverviewComputer.add("correct");
 			else
 				answersOverviewComputer.add("incorrect");
@@ -91,7 +100,6 @@ public class Play extends Controller {
 		
 		DynamicForm requestData = Form.form().bindFromRequest();
 		
-
 		List<Choice> chosenChoices = new ArrayList<Choice>();
 		for(int i = 0; i < allChoices.size(); i++) //Speichere die ausgewählten Fragen ab.
 		{
@@ -112,6 +120,20 @@ public class Play extends Controller {
 		
 		round.answerCurrentQuestion(chosenChoices, time, user, factory);
 		
+		//Random answer für den Computer (wird nicht von answerCurrentQuestion automatisch gespeichert.
+		List<Choice> computerChoices = new ArrayList<Choice>();
+		Random rand = new Random();
+		boolean trueAnswer = (rand.nextInt(100) < 50);
+		
+		if(trueAnswer)
+			computerChoices = question.getCorrectChoices();
+		else
+			computerChoices = question.getAllChoices();
+		
+		int computerTime = 0 + (int)(Math.random() * ((100 - 0) + 1));
+		
+		round.answerCurrentQuestion(computerChoices, computerTime, game.getPlayers().get(1), factory);
+		
 		if(game.isRoundOver() || questionCounter == 3)
 			return finish();
 		
@@ -122,9 +144,52 @@ public class Play extends Controller {
 	
 	public static Result finish(){
 		//TODO implement me!
+		User user = game.getPlayers().get(0);
+		User computer = game.getPlayers().get(1);
+		
 		if(game.isGameOver())
-			return ok(quizover.render());
-		return ok(roundover.render());
+		{
+			return ok(quizover.render(game, user, computer));
+		}else
+		{
+			List<String> answersOverviewUser = new ArrayList<String>();
+			
+			int i = 0;
+			while(i < questionCounter) //Hole die Antworten der schon gegebenen fragen
+			{
+				if(round.getAnswer(i, user).isCorrect())
+					answersOverviewUser.add("correct");
+				else
+					answersOverviewUser.add("incorrect");
+				i++;
+			}
+			while(i < 3) //Die Fragen die noch nicht beantwortet wurden sind unknown.
+			{
+				answersOverviewUser.add("unknown");
+				i++;
+			}
+			
+			List<String> answersOverviewComputer = new ArrayList<String>();
+			i = 0;
+			while(i < questionCounter) //Hole die Antworten der schon gegebenen fragen
+			{
+				if(round.getAnswer(i, game.getPlayers().get(1)).isCorrect())
+					answersOverviewComputer.add("correct");
+				else
+					answersOverviewComputer.add("incorrect");
+				i++;
+			}
+			while(i < 3) //Die Fragen die noch nicht beantwortet wurden sind unknown.
+			{
+				answersOverviewComputer.add("unknown");
+				i++;
+			}
+			
+			
+			return ok(roundover.render(game, user, computer, answersOverviewUser, answersOverviewComputer));
+		}
+			
+		
 	}
 	
 }
