64ccf87f46aa3db9a8ec1161a32dfaf15b74a99f
Julian Pegoraro <julianpegoraro@gmail.com>
2014-05-24 15:54:21 +0200

    quizuser id geloescht

--- we-lab4-group26/app/models/QuizUser.java
+++ we-lab4-group26/app/models/QuizUser.java
@@ -5,9 +5,6 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 import play.data.validation.Constraints;
 import play.data.validation.ValidationError;
@@ -20,8 +17,6 @@ public class QuizUser extends BaseEntity {
 	}
 
     //@TODO - as soon as the id attribute has been introduced to the base entity - remove the ID
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 
