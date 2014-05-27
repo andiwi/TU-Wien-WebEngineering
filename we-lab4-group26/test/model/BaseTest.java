1d358c93dd351c72c600051787d4f9806b2ec78c
Andreas Wittmann <e1225854@student.tuwien.ac.at>
2014-05-27 18:54:12 +0200

    jUnit Test läuft jetzt (es fehlen aber noch die restlichen Tests!)

--- we-lab4-group26/test/model/BaseTest.java
+++ we-lab4-group26/test/model/BaseTest.java
@@ -1,87 +1,58 @@
 package model;
 
-import static org.junit.Assert.fail;
-import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.start;
-
 import javax.persistence.EntityManager;
 
-import org.junit.After;
-import org.junit.Before;
+import models.IQuizDAO;
+import models.QuizDAO;
 
-import play.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
 import play.db.jpa.JPA;
 import play.db.jpa.JPAPlugin;
-import play.libs.F;
 import play.test.FakeApplication;
-import models.QuizDAO;
+import play.test.Helpers;
 
 public abstract class BaseTest {
 
-	final protected QuizDAO quizDao = QuizDAO.INSTANCE;
-	protected EntityManager em;
-	
+	protected static FakeApplication app;
+    protected static EntityManager em;
+    protected static IQuizDAO quizDao;
 	
 	/**
      * Setup the test
      */
-    @Before
-    public void setupTest() {
-        FakeApplication app = fakeApplication();
-        start(app);
+    @BeforeClass
+    public static void startApp() {
+        app = Helpers.fakeApplication(Helpers.inMemoryDatabase());
+        Helpers.start(app);
+
         em = app.getWrappedApplication().plugin(JPAPlugin.class).get().em("default");
         JPA.bindForCurrentThread(em);
 
+        quizDao = QuizDAO.INSTANCE;
+    }
 
-        emptyDatabase();
+    @AfterClass
+    public static void stopApp() {
+        quizDao = null;
 
+        JPA.bindForCurrentThread(null);
+        em.close();
+        em = null;
 
+        Helpers.stop(app);
+    }
+
+    @Before
+    public void setUp() {
+        em.getTransaction().begin();
     }
 
     @After
-    public void tearDownIntegrationTest() {
-        JPA.bindForCurrentThread(null);
-        em.close();
-    }
-
-
-    /**
-     * Since we use a file-based database and not an in-memory database for this showcase we must
-     * remove any old entries.
-     */
-    @Before
-    public void emptyDatabase() {
-
-        try  {
-            JPA.withTransaction(new F.Callback0() {
-                @Override
-                public void invoke() throws Throwable {
-
-                    em.getTransaction().begin();
-
-                    Logger.info("Emptying database");
-                    int count = em.createQuery("DELETE FROM Category").executeUpdate();
-                    Logger.info("Deleted {} Categorys", count);
-
-                    count = em.createQuery("DELETE FROM Choice").executeUpdate();
-                    Logger.info("Deleted {} Choices", count);
-
-                    count = em.createQuery("DELETE FROM Question").executeUpdate();
-                    Logger.info("Deleted {} questions", count);
-
-                    count = em.createQuery("DELETE FROM Quizuser").executeUpdate();
-                    Logger.info("Deleted {} Quizusers", count);
-
-                    em.getTransaction().commit();
-
-                }
-            });
-
-        }
-        catch (Exception e) {
-            Logger.error("Test failed", e);
-            fail();
-        }
-
+    public void tearDown() {
+        em.getTransaction().rollback();
     }
 }
--- we-lab4-group26/test/model/QuizDAOTest.java
+++ we-lab4-group26/test/model/QuizDAOTest.java
@@ -1,58 +1,20 @@
 package model;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import static org.junit.Assert.assertNotNull;
 import models.Category;
-import models.Choice;
-import models.Question;
 
 import org.junit.Test;
 
-import play.db.jpa.JPA;
-import play.libs.F;
-
 public class QuizDAOTest extends BaseTest{
 
-	@Test
-    public void testPersistCategories() {
-
-        JPA.withTransaction(new F.Callback0() {
-            @Override
-            public void invoke() throws Throwable {
-            	List<Question> questions = new ArrayList<Question>();
-            	Question q = new Question();
-            	q.setTextDE("textDE");
-            	q.setTextEN("textEN");
-            	
-            	List<Choice> choices = new ArrayList<Choice>();
-            	for (int i = 0; i < 3; i++)
-            	{
-	            	Choice choice = new Choice();
-	            	choice.setCorrectAnswer(true);
-	            	choice.setTextDE("textDE"+i);
-	            	choice.setTextEN("textEN"+i);
-	            	choice.setQuestion(questions.get(0));
-            	}
-            	
-            	q.setChoices(choices);
-            	            	
-                for (int i = 0; i < 3; i++) {
-                    Category c = new Category();
-                    c.setNameDE("nameDE"+i);
-                    c.setNameEN("nameEN"+i);
-                    c.setQuestions(questions);
-                   
-                    quizDao.merge(c);
-                }
-
-                List<Category> categories = quizDao.findEntities(Category.class);
-                assertTrue(categories.size() == 3);
-
-            }
-        });
-
+    @Test
+    public void persistCategory() {
+        Category c = new Category();
+        c.setNameDE("Webentwicklung");
+        c.setNameEN("Web-Engineering");
+        quizDao.persist(c);
+        em.flush();
+        assertNotNull(c.getId());
     }
+	
 }
